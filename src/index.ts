7c22f299ec0029f8e818d120f65d834df17ea56e
Aurélien Leygues <aurelien.leygues@gmail.com>
2024-09-24 15:02:01 +0200

    initial commit (express API with GET + POST)

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import express from "express";
+
+type Ad = {
+  id: number;
+  title: string;
+  description: string;
+  owner: string;
+  price: number;
+  picture: string;
+  location: string;
+  createdAt: string;
+};
+
+const app = express();
+
+app.use(express.json());
+
+const ads: Ad[] = [
+  {
+    id: 1,
+    title: "Bike to sell",
+    description:
+      "My bike is blue, working fine. I'm selling it because I've got a new one",
+    owner: "bike.seller@gmail.com",
+    price: 100,
+    picture:
+      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+    location: "Paris",
+    createdAt: "2023-09-05T10:13:14.755Z",
+  },
+  {
+    id: 2,
+    title: "Car to sell",
+    description:
+      "My car is blue, working fine. I'm selling it because I've got a new one",
+    owner: "car.seller@gmail.com",
+    price: 10000,
+    picture:
+      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+    location: "Paris",
+    createdAt: "2023-10-05T10:14:15.922Z",
+  },
+];
+
+app.get("/ads", (req, res) => {
+  res.json(ads);
+});
+
+app.post("/ads", (req, res) => {
+  const ad: Ad = req.body;
+  ad.id = ads[ads.length - 1].id + 1;
+  ads.push(ad);
+  res.json({
+    id: ad.id,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000 🚀");
+});
