4bee4110d196f803710567e4c0068b2081cfaa3d
Aurélien Leygues <aurelien.leygues@gmail.com>
2024-11-18 15:25:07 +0100

    fixing ad update

--- backend/src/resolvers/Ads.ts
+++ backend/src/resolvers/Ads.ts
@@ -1,6 +1,7 @@
 import { Arg, ID, Mutation, Query, Resolver } from "type-graphql";
 import { Ad, AdCreateInput, AdUpdateInput } from "../entities/Ad";
 import { validate } from "class-validator";
+import { merge } from "../utils/merge";
 
 @Resolver()
 export class AdsResolver {
@@ -52,19 +53,16 @@ export class AdsResolver {
     @Arg("id", () => ID) id: number,
     @Arg("data", () => AdUpdateInput) data: AdUpdateInput
   ): Promise<Ad | null> {
-    const ad = await Ad.findOneBy({ id });
+    const ad = await Ad.findOne({ where: { id }, relations: { tags: true } });
     if (ad !== null) {
-      Object.assign(ad, data);
-
-      // bug → ad.tags
-      // { id: 1 } → unicity constraint
-      // we should should replace this object with a real tag
-      // Tag { id: 1 } → no bug here
+      merge(ad, data);
 
       const errors = await validate(ad);
+
       if (errors.length > 0) {
         throw new Error(`Validation error: ${JSON.stringify(errors)}`);
       } else {
+        console.log(ad);
         await ad.save();
         return ad;
       }
