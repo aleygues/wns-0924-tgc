e9a27669854b2e384a24af5a46f5687d02bb9c83
Aurélien Leygues <aurelien.leygues@gmail.com>
2024-12-18 16:51:05 +0100

    adding dockerignore + database env file

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -4,9 +4,9 @@ export const datasource = new DataSource({
   type: "postgres",
   host: "db", // 172.12.10.10
   port: 5432,
-  username: "thegoodcorner",
-  password: "supersecret",
-  database: "thegoodcorner",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ["./src/entities/*.ts"],
   synchronize: true,
   logging: true,
