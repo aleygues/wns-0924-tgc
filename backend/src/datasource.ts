cf723ec7c4404585a1851bcd20ea9cb74db23b13
Aurélien Leygues <aurelien.leygues@gmail.com>
2024-12-18 14:36:14 +0100

    adding postgresql

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,8 +1,12 @@
 import { DataSource } from "typeorm";
 
 export const datasource = new DataSource({
-  type: "sqlite",
-  database: "database/db.sqlite",
+  type: "postgres",
+  host: "db", // 172.12.10.10
+  port: 5432,
+  username: "thegoodcorner",
+  password: "supersecret",
+  database: "thegoodcorner",
   entities: ["./src/entities/*.ts"],
   synchronize: true,
   logging: true,
